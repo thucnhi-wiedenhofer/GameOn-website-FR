4501f57adbd4952420d27535e88014424be9994c
thuc nhi <thuc-nhi.wiedenhofer@laolateforme.io>
2022-01-24 13:47:30 +0100

    issue 1 fermeture modal ok

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModal = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,10 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal form
+closeModal.addEventListener("click", closeForm);
 
+//Close modal form
+function closeForm() {
+  modalbg.style.display = "none";
+}

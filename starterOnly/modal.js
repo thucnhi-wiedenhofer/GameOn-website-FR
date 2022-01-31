c4fdc020907e4889d4e5782842309c257197ae44
thuc nhi <thuc-nhi.wiedenhofer@laolateforme.io>
2022-01-31 15:39:36 +0100

    form validation  ok

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -38,35 +38,25 @@ function closeWindow() {
   modalThanks.style.display = "none";
 }
 
-// When registration form is submited
-submit.addEventListener("click", registration);
-
 //Modal thanks for registration open
 function registration() {
   modalThanks.style.display = "block";
   modalbg.style.display = "none";
 }
 
-//Validation form: when click on submit button, the form is validate if no data error on input
-/*const inputFirst = document.getElementById("first");
-const inputLast = document.getElementById("last");
-const inputEmail = document.getElementById("email");
-const inputBirthdate = document.getElementById("birthdate");
-const inputQuantity = document.getElementById("quantity");*/
+let input = document.querySelector("form");
 
-let form = document.querySelector("form");
-
-//verify if firstname has min 2 characters and no numeric value
+//verify if name has min 2 characters and no numeric value
 function validName(text) {
   return /^[a-zA-Zéèàê' -]{2,}$/.test(text);
 }
 
 //********Check firstname field******
-form.first.addEventListener("change", function () {
+input.first.addEventListener("input", function () {
   validName(this);
-  console.log(validName(form.first.value));
+  console.log(validName(input.first.value));
 
-  if (!validName(form.first.value)) {
+  if (!validName(input.first.value)) {
     formData[0].setAttribute("data-error-visible", "true");
     return false;
   } else {
@@ -76,11 +66,11 @@ form.first.addEventListener("change", function () {
 });
 
 //********Check lastname field******
-form.last.addEventListener("change", function () {
+input.last.addEventListener("input", function () {
   validName(this);
-  console.log(validName(form.last.value));
+  console.log(validName(input.last.value));
 
-  if (!validName(form.last.value)) {
+  if (!validName(input.last.value)) {
     formData[1].setAttribute("data-error-visible", "true");
     return false;
   } else {
@@ -90,16 +80,16 @@ form.last.addEventListener("change", function () {
 });
 
 //********Check email field******
-form.email.addEventListener("change", function () {
+input.email.addEventListener("input", function () {
   validEmail(this);
-  console.log(validEmail(form.email.value));
+  console.log(validEmail(input.email.value));
   //verify if email field is not empty
-  if (form.email.value === "") {
+  if (input.email.value === "") {
     formData[2].setAttribute("data-error", "le champs est vide");
     formData[2].setAttribute("data-error-visible", "true");
     return false;
     //verify if email is valid
-  } else if (!validEmail(form.email.value)) {
+  } else if (!validEmail(input.email.value)) {
     formData[2].setAttribute("data-error-visible", "true");
     return false;
   } else {
@@ -116,16 +106,16 @@ function validEmail(email) {
 }
 
 //********Check birthdate field******
-form.birthdate.addEventListener("change", function () {
+input.birthdate.addEventListener("change", function () {
   validBirthdate(this);
-  console.log(validBirthdate(form.birthdate.value));
+  console.log(validBirthdate(input.birthdate.value));
   //verify if birthdate field is not empty
-  if (form.birthdate.value === "jj/mm/aaaa") {
+  if (input.birthdate.value === "jj/mm/aaaa") {
     formData[3].setAttribute("data-error", "le champs est vide");
     formData[3].setAttribute("data-error-visible", "true");
     return false;
     //verify if birthdate is passed
-  } else if (!validBirthdate(form.birthdate.value)) {
+  } else if (!validBirthdate(input.birthdate.value)) {
     formData[3].setAttribute("data-error-visible", "true");
     return false;
   } else {
@@ -135,8 +125,7 @@ form.birthdate.addEventListener("change", function () {
 });
 
 function validBirthdate(birthdate) {
-  const date = new Date(form.birthdate.value);
-
+  const date = new Date(input.birthdate.value);
   if (date.getTime() < Date.now()) {
     return true;
   } else {
@@ -144,58 +133,21 @@ function validBirthdate(birthdate) {
   }
 }
 
-/*
+//********Function when click on button "c'est parti!"******
 function validate() {
-  const data = new FormData(document.querySelector("form"));
-
-  //verify if firstname has min 2 characters and no numeric value
-  if (inputFirst.value.length < 2 && !isOnlyAlphabet(inputFirst.value)) {
-    formData[0].setAttribute("data-error-visible", "true");
-    return false;
+  if (
+    validName(input.first.value) &&
+    validName(input.last.value) &&
+    validEmail(input.email.value) &&
+    validBirthdate(input.birthdate.value) &&
+    //check if one location value was checked
+    input.location.value &&
+    //check if term and condition was checked
+    input.checkbox1.checked
+  ) {
+    //Registration is valid and thanks modal open
+    registration();
   } else {
-    formData[0].setAttribute("data-error-visible", "false");
+    alert("veuillez renseigner tous les champs");
   }
-  //verify if lastname has min 2 characters and no numeric value
-  if (inputLast.value.length < 2 && !isOnlyAlphabet(inputLast.value)) {
-    formData[1].setAttribute("data-error-visible", "true");
-    return false;
-  } else {
-    formData[1].setAttribute("data-error-visible", "false");
-  }
-
-  //verify if email field is not empty
-  if (inputEmail.value === "") {
-    formData[2].setAttribute("data-error-visible", "true");
-    return false;
-  } else {
-    formData[2].setAttribute("data-error-visible", "false");
-  }
-
-  //verify if quantity field is not empty
-  if (inputQuantity.value === "") {
-    formData[4].setAttribute("data-error-visible", "true");
-    return false;
-  } else {
-    formData[4].setAttribute("data-error-visible", "false");
-  }
-
-  //verify if all attributes name=location of checkboxes was checked
-  if (!data.has("location")) {
-    formData[5].setAttribute("data-error-visible", "true");
-    return false;
-  } else {
-    formData[5].setAttribute("data-error-visible", "false");
-  }
-
-  //verify if attribute name=checkbox1 was checked
-  if (!data.has("checkbox1")) {
-    formData[6].setAttribute("data-error-visible", "true");
-    return false;
-  } else {
-    formData[6].setAttribute("data-error-visible", "false");
-  }
-
-  
- registration();
 }
-*/
